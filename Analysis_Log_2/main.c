a8b19a681e4db0a05e17d36550d61fdd71749593
conghuy179 <105105177+conghuy179@users.noreply.github.com>
2022-12-08 23:32:26 +0700

    Finish cau 1

--- Analysis_Log_2/main.c
+++ Analysis_Log_2/main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <math.h>
+
 #define FILENAME "log.txt"
 #define MAXLENGTHFILE 5000
 
@@ -24,7 +28,7 @@ int file_to_str (char* str)
     return status;
 }
 
-int count_messages(char *str, char *phase)
+int count_messages(char *str, char *phase) //Ham dem ban tin
 {
     // khoi tao bien dem
     int count = 0;
@@ -40,16 +44,63 @@ int count_messages(char *str, char *phase)
     return count;
 }
 
+int count_messages_from_device(char *file_str, char *network_address) //ham trich xuat ban tin tu file log
+{
+
+    char info[18][100]; //co 18 info
+    int i = 0;
+
+     //Cat chuoi JSON bang dau ","
+    char *token = strtok(file_str, "\n");
+
+
+   // Tiep tuc cat chuoi cho den khi duoc 18 doan gia tri
+    while (token != NULL)
+    {
+        printf("Token = %s\n", token);
+        strcpy(info[i], token);
+        printf("Info[%d] = %s\n", i, info[i]);
+        i++;
+        token = strtok(NULL, "\n");
+    }
+
+    // khai bao cac bien can thiet
+
+    char command_phase[] = "\"cmd\":\"set\"";
+    int message_count = 0;
+    char *temp1, *temp2;
+    int info_len = strlen(info);
+
+     // dem command phase va network address trong info
+     // neu tim thay ca 2, count++
+
+    temp1 = strstr(info,command_phase);
+    temp2 = strstr(info, network_address);
+
+    if (temp1 != NULL && temp2 != NULL)
+    {
+        message_count++;
+    }
+
+    token = strtok(NULL, "\n");
+
+    return message_count;
+
+}
 
 int main()
 {
+    // Cau 2
+    char network_address[100];
+    printf("Nhap dia chi network: \n");
+    scanf("%d", network_address);
+    int sent_count = count_messages_from_device(file_str,network_address);
+    printf("So ban tin gui di: %d", sent_count);
+
+    // Cau 1:
     file_to_str(file_str);
     int slbt = count_messages(file_str, "\"cmd\":\"set\"");
-    printf("So luong ban tin: %d", slbt);
+    printf("So luong ban tin: %d\n", slbt);
+
     return 0;
-
-    // cau 2
-
-    printf("Nhap ma network: \n");
-    scanf("%d", )
 }
